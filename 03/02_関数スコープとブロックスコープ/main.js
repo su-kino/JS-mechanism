f6f8cf1cf10315f96540daa36ab3a339bcb33518
su-kino <97873142+su-kino@users.noreply.github.com>
2022-05-17 15:12:20 +0900

    [practice]03

---
+++ 03/02_関数スコープとブロックスコープ/main.js
@@ -0,0 +1,17 @@
+function a() {
+  let b = 0;
+  console.log(b);
+}
+a();
+
+if(true) {
+  var c = 1;
+
+  var d = function () {
+    console.log('d is called');
+  }
+
+}
+
+d();
+console.log(c);
---
+++ 03/07_即時関数/main.js
@@ -0,0 +1,34 @@
+function a() {
+  console.log('called');
+}
+
+a();
+
+let c = (function() {
+
+  console.log('called');
+  let privateVal = 0;
+  let publicVal = 10;
+
+  function privateFn() {
+    console.log('privateFn is called');
+  }
+  function publicFn() {
+    console.log('publicFn is called' + privateVal++);
+  }
+
+  return {
+    publicVal,
+    publicFn
+  };
+})();
+
+c.publicFn();
+c.publicFn();
+c.publicFn();
+
+console.log(c.publicVal);
+
+let b = function() {
+  console.log('called');
+}();
