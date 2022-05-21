1fcae8d9f971c6ca4ec8ba4213c5ccc6ffd83657
su-kino <97873142+su-kino@users.noreply.github.com>
2022-05-21 11:47:45 +0900

    [practice]05(01~05)

---
+++ 05/05_参照のコピーとthis/main.js
@@ -0,0 +1,18 @@
+window.name = 'John';
+
+const person = {
+    name: 'Tom',
+    hello: function() {
+        console.log('Hello ' + this.name);
+        a();
+    }
+}
+
+// const ref = person.hello;
+// ref();
+
+person.hello();
+
+function a() {
+    console.log('Hello ' + this.name);
+}
